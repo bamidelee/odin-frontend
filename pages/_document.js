aee06a75bc2915347038a158f470da81f0405039
bamidele <ibrahimolujide@gmail.com>
2023-02-24 23:57:49 +0100

    ads

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,16 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9120505021602496"
-          crossorigin="anonymous">
-        </script>
+      <Script
+          async
+          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9120505021602496`}
+          strategy="lazyOnload"
+          crossOrigin="anonymous"
+        />
       </Head>
       <body>
         <Main />
