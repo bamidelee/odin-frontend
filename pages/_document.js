f609fb9f30631315919047169d627309be364245
bamidele <ibrahimolujide@gmail.com>
2023-03-03 15:18:41 +0100

    adstargets remove

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-      <meta name="admverifysite" content="2d45f3d66b05a54863a9bcadfeadf867" />
+     
       </Head>
       <body>
         <Main />
