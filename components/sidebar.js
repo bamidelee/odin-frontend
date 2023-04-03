05d5b7ff24b083b7ac09536b19acb440c0c3e1e8
bamidele <ibrahimolujide@gmail.com>
2023-04-03 21:09:28 +0100

    new

--- components/sidebar.js
+++ components/sidebar.js
@@ -15,17 +15,56 @@ export default function SideBar(props) {
     return(
         <div className= {props.sideBar? `${styles.sidebar} ${styles.open}`: `${styles.sidebar} ${styles.closed}`}>
           
-          <Link href="/" replace onClick={() =>props.setSideBar(false)}><Icon path={mdiHome} size={1} />Home</Link>
-          <Link href="/movies"  onClick={() =>props.setSideBar(false)}><Icon path={mdiFilmstrip} size={1} />Movies</Link>
+          <Link href="/" replace onClick={() =>props.setSideBar(false)}><Icon path={mdiHome} size={1} /> 
+            <div  className={styles.link}>
+              <span className={styles.linkTop}>Home</span>
+              <span class={styles.linkBottom}>Home</span>
+            </div>
+          </Link>
+          <Link href="/movies"  onClick={() =>props.setSideBar(false)}><Icon path={mdiFilmstrip} size={1} />
+          <div  className={styles.link}>
+              <span className={styles.linkTop}>Movies</span>
+              <span class={styles.linkBottom}>Movies</span>
+            </div>
+          </Link>
           <button onClick={() => setNewsOpen(!newsOpen)}><Icon path={mdiNewspaper} size={1} />News <Icon path={mdiChevronDown} size={1} /></button>
           <div className={styles.newsLinks} style= {{display: newsOpen? 'flex':'none'}}>
-            <Link href="/page/football/1/post"  onClick={() =>props.setSideBar(false)}>Football</Link>
-            <Link href="/page/entertainment/1/post" onClick={() =>props.setSideBar(false)}> Entertainment</Link>
-            <Link  href="/page/politics/1/post"  onClick={() =>props.setSideBar(false)}>Politics</Link>
-            <Link  href="/page/international/1/post"  onClick={() =>props.setSideBar(false)}>International</Link>
+            <Link href="/page/football/1/post"  onClick={() =>props.setSideBar(false)}>
+            <div  className={styles.link}>
+              <span className={styles.linkTop}>Football</span>
+              <span class={styles.linkBottom}>Football</span>
+            </div>
+            </Link>
+            <Link href="/page/entertainment/1/post" onClick={() =>props.setSideBar(false)}>
+            <div  className={styles.link}>
+              <span className={styles.linkTop}>Entertainment</span>
+              <span class={styles.linkBottom}>Entertainment</span>
+            </div>
+            </Link>
+            <Link  href="/page/politics/1/post"  onClick={() =>props.setSideBar(false)}>
+            <div  className={styles.link}>
+              <span className={styles.linkTop}>Politics</span>
+              <span class={styles.linkBottom}>Politics</span>
+            </div>
+            </Link>
+            <Link  href="/page/international/1/post"  onClick={() =>props.setSideBar(false)}>
+            <div  className={styles.link}>
+              <span className={styles.linkTop}>International</span>
+              <span class={styles.linkBottom}>International</span>
+            </div>
+            </Link>
           </div>
-          <Link href="/fixtures"  onClick={() =>props.setSideBar(false)}><Icon path={mdiSoccerField} size={1} />Fixtures</Link>
-          <Link href="/tables"  onClick={() =>props.setSideBar(false)}><Icon path={mdiTable} size={1} />Tables</Link>
+          <Link href="/fixtures"  onClick={() =>props.setSideBar(false)}><Icon path={mdiSoccerField} size={1} />
+          <div  className={styles.link}>
+              <span className={styles.linkTop}>Fixtures</span>
+              <span class={styles.linkBottom}>Fixtures</span>
+            </div></Link>
+          <Link href="/tables"  onClick={() =>props.setSideBar(false)}><Icon path={mdiTable} size={1} />
+          <div  className={styles.link}>
+              <span className={styles.linkTop}>Tables</span>
+              <span class={styles.linkBottom}>Tables</span>
+            </div>
+          </Link>
         </div>
     )
 }
