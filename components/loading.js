46c29af318ee7a4b20adbe524a52b0ef114938a3
bamidele <ibrahimolujide@gmail.com>
2023-04-03 10:52:06 +0100

    new

--- components/loading.js
+++ components/loading.js
@@ -9,7 +9,7 @@ export default function Loading() {
   
     useEffect(() => {
         const handleStart = (url) => (url !== router.asPath) && setLoading(true);
-        const handleComplete = (url) => (url === router.asPath) && setTimeout(() =>{setLoading(false)},2000);
+        const handleComplete = (url) => (url === router.asPath) && setLoading(false);
   
         router.events.on('routeChangeStart', handleStart)
         router.events.on('routeChangeComplete', handleComplete)
