e88c330d65b95a960c9a78b6467c2200cd337aa7
bamidele <ibrahimolujide@gmail.com>
2023-04-06 15:20:11 +0100

    new

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
       <meta name="exoclick-site-verification" content="36f890cd1c58742a61865fd15cf4487a"></meta>
+      <meta name="description" content="News and movie download website"></meta>
       <link rel="icon" href="/titleIcon.jpg/" />
       </Head>
       <body>
