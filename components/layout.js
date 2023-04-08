de07ab70b948049dacf906f6f9c845b1c189561f
bamidele <ibrahimolujide@gmail.com>
2023-04-08 20:02:01 +0100

    update

--- components/layout.js
+++ components/layout.js
@@ -16,14 +16,15 @@ import Banner from "./banner";
 
 export default function Layout({ children }) {
 
-  const [desktopBanner, setDesktopBanner] = useState(false)
+  const [bigScreen, setBigScreen] = useState(false)
 
   useEffect(() => {
 
-    if (window.innerWidth > 650) {
-      setDesktopBanner(true)
+    if (window.innerWidth > 650 ) {
+      setBigScreen(true)
     }
   },[])
+
 
   const [sideBar, setSideBar] = useState(false)
   const ref = useRef(null)
@@ -40,19 +41,20 @@ export default function Layout({ children }) {
         <SideBar sideBar={sideBar} setSideBar={setSideBar} />
 
         <main ref={ref} style={{ paddingTop: '4rem', width: '100%' }} onClick={(() => setSideBar(false))}>
-          {desktopBanner && <Banner slot='8c47067f1ac7389ef98d7ba0c597c9d9' />}
+         {bigScreen && <Banner slot='8c47067f1ac7389ef98d7ba0c597c9d9' />}
+         {bigScreen && <Banner slot='8c47067f1ac7389ef98d7ba0c597c9d9' />}
           <Link href='https://t.me/thenaijaodin' className="telegram">
             <Icon path={mdiSend} size={1.5} />
             Join our telegram channel
           </Link>
 
           {children}
-          {desktopBanner && <div>
+          {bigScreen && <div>
             <Script async="async" data-cfasync="false" src="//pl18660884.highrevenuegate.com/1e845c512aba6f843b89be278fa82a95/invoke.js"></Script>
             <div id="container-1e845c512aba6f843b89be278fa82a95"></div>
           </div>}
 
-          {!desktopBanner && <DesktopBanner slot='1d24a5888bd79927cba80711f10c599a' />
+          {!bigScreen && <DesktopBanner slot='1d24a5888bd79927cba80711f10c599a' />
           }
 
         </main>
