0963a659ffc0c57bb866a8ce07a08cdf7af53141
bamidele <ibrahimolujide@gmail.com>
2023-06-24 13:20:39 +0100

    new

--- pages/_document.js
+++ pages/_document.js
@@ -7,8 +7,6 @@ export default function Document() {
       <Head>
         <meta name="exoclick-site-verification" content="36f890cd1c58742a61865fd15cf4487a"></meta>
         <meta name="description" content="News and movie download website"></meta>
-        <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9120505021602496"
-          crossorigin="anonymous"></Script>
         <link rel="icon" href="/titleIcon.jpg/" />
       </Head>
       <body>
